cbb6890c6491f85039b77155c2dfdd6014286d57
woodun <hwang07@email.wm.edu>
2020-03-29 01:47:16 -0400

    debug

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -128,6 +128,7 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_hello]   sys_hello, /////////////////myedit
+[SYS_hello]   sys_helloYou, /////////////////myedit
 };
 
 void
--- xv6-public/syscall.h
+++ xv6-public/syscall.h
@@ -21,3 +21,4 @@
 #define SYS_mkdir  20
 #define SYS_close  21
 #define SYS_hello  22 /////////////////myedit
+#define SYS_helloYou  23 /////////////////myedit
