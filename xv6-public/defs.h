dcc915362ee32f5dfc4a37301e7c92b506c49d4e
woodun <hwang07@email.wm.edu>
2020-03-29 01:44:08 -0400

    debug

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -122,6 +122,7 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int             hello(void); /////////////////myeditd
+int             helloYou(char*); /////////////////myeditd
 
 // swtch.S
 void            swtch(struct context**, struct context*);
