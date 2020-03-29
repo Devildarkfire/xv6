af56248031a8d702a5642c2280254a1572161435
woodun <hwang07@email.wm.edu>
2020-03-29 01:48:33 -0400

    debug

--- xv6-public/syscall.c
+++ xv6-public/syscall.c
@@ -104,6 +104,7 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_hello(void); /////////////////myedit
+extern int sys_helloYou(void); /////////////////myedit
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -128,7 +129,7 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_hello]   sys_hello, /////////////////myedit
-[SYS_hello]   sys_helloYou, /////////////////myedit
+[SYS_helloYou]   sys_helloYou, /////////////////myedit
 };
 
 void
