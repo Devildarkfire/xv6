1bae24d266a90bdeac087438b1918edf1e3813ad
woodun <hwang07@email.wm.edu>
2020-04-20 10:21:24 -0400

    debug

--- grading/student_modified_files/kaichenzhang/proc.h
+++ grading/student_modified_files/kaichenzhang/proc.h
@@ -50,6 +50,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   int number_ContextSwitches;
+  int prio;
 };
 
 // Process memory is laid out contiguously, low addresses first:
